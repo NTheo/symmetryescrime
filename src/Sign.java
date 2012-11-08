d3449e0749650402cb59352767edc3b8984560ba
mrshadow.night <mrshadow.night@18363ba7-644b-40b0-f81c-52608aade5a2>
2012-11-08 21:02:31 +0000

    first try on abstract class sign

---
+++ src/Sign.java
@@ -0,0 +1,9 @@
+import Jcg.geometry.Point_3;
+import Jcg.polyhedron.Vertex;
+
+
+public abstract class Sign {
+	Sign(){}
+	abstract void computeSign(SurfaceMesh m, Vertex<Point_3> v, float radius);
+	abstract boolean isValidSign(); 
+}
