cc61623a079e37a6a47d378361e87393d1dd4c2d
mrshadow.night <mrshadow.night@18363ba7-644b-40b0-f81c-52608aade5a2>
2012-11-08 21:08:07 +0000

    corrected "isValid" name

--- trunk/src/Sign.java
+++ trunk/src/Sign.java
@@ -5,5 +5,5 @@ import Jcg.polyhedron.Vertex;
 public abstract class Sign {
 	Sign(){}
 	abstract void computeSign(SurfaceMesh m, Vertex<Point_3> v, float radius);
-	abstract boolean isValidSign(); 
+	abstract boolean isValid(); 
 }
