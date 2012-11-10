59cda50419d7992560bfe8101525f08ba2a403f9
ponsard.antoine <ponsard.antoine@18363ba7-644b-40b0-f81c-52608aade5a2>
2012-11-10 15:18:27 +0000

    minor changes

--- src/MeshViewer.java
+++ src/MeshViewer.java
@@ -1,6 +1,5 @@
 import processing.core.*;
 
-import Jcg.geometry.*;
 
 /**
  * A simple 3d viewer for visualizing surface meshes
@@ -14,40 +13,41 @@ public class MeshViewer extends PApplet {
 	SurfaceMesh mesh;
 	//String filename="OFF/tri_round_cube.off";
 	String filename="OFF/tri_triceratops.off";
-	
+
 	public void setup() {
-		  size(800,600,P3D);
-		  @SuppressWarnings("unused")
-		  ArcBall arcball = new ArcBall(this);
-		  
-		  this.mesh=new SurfaceMesh(this, filename);
-		  this.mesh.polyhedron3D.isValid(false);
+		size(800,600,P3D);
+
+		@SuppressWarnings("unused")
+		ArcBall arcball = new ArcBall(this);
+
+		this.mesh=new SurfaceMesh(this, filename);
+		this.mesh.polyhedron3D.isValid(false);
 	}
-		 
-		public void draw() {
-		  background(0);
-		  //this.lights();
-		  directionalLight(101, 204, 255, -1, 0, 0);
-		  directionalLight(51, 102, 126, 0, -1, 0);
-		  directionalLight(51, 102, 126, 0, 0, -1);
-		  directionalLight(102, 50, 126, 1, 0, 0);
-		  directionalLight(51, 50, 102, 0, 1, 0);
-		  directionalLight(51, 50, 102, 0, 0, 1);
-		 
-		  translate(width/2.f,height/2.f,-1*height/2.f);
-		  this.strokeWeight(1);
-		  stroke(150,150,150);
-		  
-		  this.mesh.draw();
-		}
-		
-		/**
-		 * For running the PApplet as Java application
-		 */
-		public static void main(String args[]) {
-			//PApplet pa=new MeshViewer();
-			//pa.setSize(400, 400);
-			PApplet.main(new String[] { "MeshViewer" });
-		}
+
+	public void draw() {
+		background(0);
+		//this.lights();
+		directionalLight(101, 204, 255, -1, 0, 0);
+		directionalLight(51, 102, 126, 0, -1, 0);
+		directionalLight(51, 102, 126, 0, 0, -1);
+		directionalLight(102, 50, 126, 1, 0, 0);
+		directionalLight(51, 50, 102, 0, 1, 0);
+		directionalLight(51, 50, 102, 0, 0, 1);
+
+		translate(width/2.f,height/2.f,-1*height/2.f);
+		this.strokeWeight(1);
+		stroke(150,150,150);
+
+		this.mesh.draw();
+	}
+
+	/**
+	 * For running the PApplet as Java application
+	 */
+	public static void main(String args[]) {
+		//PApplet pa=new MeshViewer();
+		//pa.setSize(400, 400);
+		PApplet.main(new String[] { "MeshViewer" });
+	}
 
 }
