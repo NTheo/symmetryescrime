447fb7960189653be1918aa80506e0a78326bed8
ponsard.antoine <ponsard.antoine@18363ba7-644b-40b0-f81c-52608aade5a2>
2012-11-11 17:49:53 +0000

    framework sampling

---
+++ trunk/src/Parameters.java
@@ -0,0 +1,12 @@
+
+/**
+ * All the magic numbers required
+ * @author Antoine & NTheo (2012)
+ *
+ */
+public class Parameters {
+
+	static final double samplingRatio=0.1;
+	static final double pruningThreshold=0.75;
+	static final double curvatureCalculationRadius=10.;
+}
---
+++ trunk/src/Sampling.java
@@ -0,0 +1,26 @@
+import java.util.LinkedList;
+
+import Jcg.geometry.Point_3;
+import Jcg.polyhedron.Vertex;
+
+
+/**
+ * Abstract class for extracting a sample of vertices from the model 
+ * @author Antoine & NTheo (2012)
+ *
+ */
+public abstract class Sampling {
+
+	// NB: we should use Iterator.remove for the pruning, in O(1) instead of O(n) for remove()
+	protected LinkedList<Vertex<Point_3>> sample;  
+	
+	// Constructor : extract a sample of vertices from the given polyhedron
+	// Sampling(){}	
+	
+	// Display on the model the points of the sample
+	abstract void displaySample();
+	
+	LinkedList<Vertex<Point_3>> sample(){
+		return this.sample;
+	}
+}
