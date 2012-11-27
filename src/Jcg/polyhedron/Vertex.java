97440acd4ec8f9f9a9536bc8d18659c866b917d3
ponsard.antoine <ponsard.antoine@18363ba7-644b-40b0-f81c-52608aade5a2>
2012-11-27 15:46:01 +0000

    not sure

--- src/Jcg/polyhedron/Vertex.java
+++ src/Jcg/polyhedron/Vertex.java
@@ -10,23 +10,23 @@ import Jcg.geometry.*;
 
 public class Vertex{
 
-Halfedge halfedge=null;
-Point_3 point=null;
-public int tag;
-public int index;
+	Halfedge halfedge=null;
+	Point_3 point=null;
+	public int tag;
+	public int index;
 
-    public Vertex(Point_3 point) { this.point=point; }
-    public Vertex() {}
+	public Vertex(Point_3 point) { this.point=point; }
+	public Vertex() {}
 
-    public void setEdge(Halfedge halfedge) { this.halfedge=halfedge; }
-    public void setPoint(Point_3 point) { this.point=point; }  
-    
-    public Point_3 getPoint() { return this.point; } 
-    public Halfedge getHalfedge() { return this.halfedge; } 
-    
-    public String toString(){
-        return "v"+point.toString();
-    }
+	public void setEdge(Halfedge halfedge) { this.halfedge=halfedge; }
+	public void setPoint(Point_3 point) { this.point=point; }  
+
+	public Point_3 getPoint() { return this.point; } 
+	public Halfedge getHalfedge() { return this.halfedge; } 
+
+	public String toString(){
+		return "v"+point.toString();
+	}
 }
 
 
