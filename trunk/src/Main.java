54bdca4876efa915cf27b8404f7ae8f0e0089555
ponsard.antoine <ponsard.antoine@18363ba7-644b-40b0-f81c-52608aade5a2>
2012-11-27 17:09:23 +0000

    IMPORTANT new structure for Main & MeshViewer

--- trunk/src/Main.java
+++ trunk/src/Main.java
@@ -5,12 +5,34 @@ import processing.core.PApplet;
  * @author Antoine & NTheo (2012)
  *
  */
-public class Main {
+@SuppressWarnings("serial")
+public class Main extends MeshViewer {
 
+	// BE CAREFUL : if you launch MeshViewer as a Java Applet, remove "src/" from the filepath
+	//String filename="src/OFF/tri_round_cube.off";
+	static String filename="src/OFF/tri_triceratops.off";
+	Sampling sample;
+	
+	// This function will monitor all the steps of our program
+	@Override
+	public void setup() {
+		super.setup();	
+		
+		this.sample = new RandomSampling(this.mesh.polyhedron3D);
+	}
+	
+	
+	@Override
+	public void draw(){
+		super.draw();
+
+		sample.display2(this);	
+		this.mesh.draw();
+	}
+	
+	
 	public static void main(String[] args) {
-		PApplet.main(new String[] { "MeshViewer"});
-		// 1 : Sampling
-		//Sampling sample = new RandomSampling();
+		PApplet.main(new String[] { "Main",filename});		
 	}
 
 }
