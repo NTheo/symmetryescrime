28d49fcbefc27dbf7f3c3c9c3b14113b8a4d4b5f
ponsard.antoine <ponsard.antoine@18363ba7-644b-40b0-f81c-52608aade5a2>
2012-11-27 18:55:50 +0000

    Display the normals after computing the signatures

--- src/SignatureMap.java
+++ src/SignatureMap.java
@@ -1,20 +1,35 @@
 import java.util.LinkedList;
 import java.util.List;
 
+import Jcg.geometry.Point_3;
+import Jcg.geometry.Vector_3;
 import Jcg.polyhedron.Vertex;
 
 /**
- * all the signatures from the sampling
+ * All the valid signatures from the sampling
  * @author Antoine & NTheo (2012)
  *
  */
 public class SignatureMap {
 	List<Signature> m = new LinkedList<Signature>();
+	
 	public SignatureMap(Sampling s){
 		for(Vertex v:s.vertices){
 			Signature sign = new Signature(v);
+			System.out.println("signature computed !");
 			if(sign.isValid())
 				m.add(new Signature(v));
+		}
+	}
+	
+	public void display(MeshViewer MV){
+		MV.fill(250f, 250f, 0f);
+		Point_3 p;
+		Vector_3 n;
+		for(Signature s : this.m){
+			p=s.getVertex().getPoint();
+			n=s.getNormale();
+			MV.mesh.drawSegment(p, new Point_3(p.x+n.x,p.y+n.y,p.z+n.z));
 		}
 	}
 }
