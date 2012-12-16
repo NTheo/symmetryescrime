0679745cfd7c966eb42d97d882b6ae0690f82f08
mrshadow.night <mrshadow.night@18363ba7-644b-40b0-f81c-52608aade5a2>
2012-12-16 13:00:41 +0000

    not working display pairing

--- trunk/src/ReflectionSpace.java
+++ trunk/src/ReflectionSpace.java
@@ -2,10 +2,8 @@ import java.util.List;
 
 
 public class ReflectionSpace extends KDTree2<Reflection> {
-
-	public ReflectionSpace(SignatureMap SM){
+	public ReflectionSpace(SignatureMap SM, MeshViewer mv){
 		super(6);
-		
 		System.out.print("Pairing...");
 		List<Signature> neighbors;
 		Reflection r;
@@ -15,11 +13,14 @@ public class ReflectionSpace extends KDTree2<Reflection> {
 					new double[]{s.getPrincipalCurvature1()+Parameters.pairingRange/2,
 					s.getPrincipalCurvature2()+Parameters.pairingRange/2});
 			System.out.print(neighbors.size()+" ");
+			mv.noStroke();
+			mv.fill(4*neighbors.size()%255, 255, 4*neighbors.size()%255);		
+			mv.mesh.drawVertex(s.getVertex().getPoint());
 			for(Signature t: neighbors){
 				r=new Reflection(s, t);
 				this.add(r.r, r);
 			}
 		}
-		System.out.print("ok");
+		System.out.println("ok");
 	}
 }
