5a2103c1a362df0c9b5024b08784aa0be1dc69ec
mrshadow.night <mrshadow.night@18363ba7-644b-40b0-f81c-52608aade5a2>
2012-12-16 17:06:14 +0000

    display pair of points

--- src/Main.java
+++ src/Main.java
@@ -59,19 +59,6 @@ public class Main extends MeshViewer {
 		Vector_3 p = n.multiplyByScalar(2);
 		Reflection test = new Reflection(new double[]{p.x,p.y,p.z,n.x,n.y,n.z});
 
-		//Point_3 p1=s1.getVertex().getPoint();
-		//Point_3 p2=s2.getVertex().getPoint();
-		
-		//this.stroke(0,0,250);
-		//Vector_3 direction = ((Vector_3) p2.minus(p1)).normalized();
-		//direction = direction.multiplyByScalar(this.mesh.scaleFactor/6);
-		//this.mesh.drawSegment(p1.plus(direction.opposite()), p2.plus(direction));
-		
-		//this.noStroke();
-		//this.fill(50, 200, 50);		
-		//this.mesh.drawVertex(p1);
-		//this.mesh.drawVertex(p2);
-
 //		Point_3 p1=s1.getVertex().getPoint();
 //		Point_3 p2=s2.getVertex().getPoint();
 //		
@@ -85,8 +72,7 @@ public class Main extends MeshViewer {
 //		this.mesh.drawVertex(p1);
 //		this.mesh.drawVertex(p2);
 
-
-		this.signatures.displayNeighborsNumber(this);
+		//this.signatures.displayNeighborsNumber(this);
 				
 		//test.display(this);
 		//this.test2.display2(this);
@@ -94,6 +80,7 @@ public class Main extends MeshViewer {
 //			this.clusters.clusters.get(i).r.display2(this);
 //		}
 		this.clusters.clusters.get(0).r.display2(this);
+
 		for(Reflection r:this.reflections.getRange(
 				new double[]{Double.MIN_VALUE, Double.MIN_VALUE, Double.MIN_VALUE, Double.MIN_VALUE, Double.MIN_VALUE, Double.MIN_VALUE},
 				new double[]{Double.MAX_VALUE, Double.MAX_VALUE, Double.MAX_VALUE, Double.MAX_VALUE, Double.MAX_VALUE, Double.MAX_VALUE}
@@ -106,6 +93,7 @@ public class Main extends MeshViewer {
 	
 	
 	public static void main(String[] args) {
+		//while(true){System.out.println("ôigbiypgiyv");}
 		PApplet.main(new String[] { "Main",filename});		
 	}
 
