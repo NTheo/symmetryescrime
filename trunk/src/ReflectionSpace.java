e6802cbccda03a0d312bb007dcd4f3475ea12c99
mrshadow.night <mrshadow.night@18363ba7-644b-40b0-f81c-52608aade5a2>
2012-12-16 13:15:11 +0000

    preview pairing

--- trunk/src/ReflectionSpace.java
+++ trunk/src/ReflectionSpace.java
@@ -2,7 +2,7 @@ import java.util.List;
 
 
 public class ReflectionSpace extends KDTree2<Reflection> {
-	public ReflectionSpace(SignatureMap SM, MeshViewer mv){
+	public ReflectionSpace(SignatureMap SM){
 		super(6);
 		System.out.print("Pairing...");
 		List<Signature> neighbors;
@@ -13,14 +13,23 @@ public class ReflectionSpace extends KDTree2<Reflection> {
 					new double[]{s.getPrincipalCurvature1()+Parameters.pairingRange/2,
 					s.getPrincipalCurvature2()+Parameters.pairingRange/2});
 			System.out.print(neighbors.size()+" ");
-			mv.noStroke();
-			mv.fill(4*neighbors.size()%255, 255, 4*neighbors.size()%255);		
-			mv.mesh.drawVertex(s.getVertex().getPoint());
 			for(Signature t: neighbors){
 				r=new Reflection(s, t);
 				this.add(r.r, r);
 			}
 		}
 		System.out.println("ok");
+	}
+	public static void display(SignatureMap SM, MeshViewer mv){
+		List<Signature> neighbors;
+		for(Signature s: SM.p){
+			neighbors = SM.q.getRange(new double[]{s.getPrincipalCurvature1()-Parameters.pairingRange/2,
+					s.getPrincipalCurvature2()-Parameters.pairingRange/2},
+					new double[]{s.getPrincipalCurvature1()+Parameters.pairingRange/2,
+					s.getPrincipalCurvature2()+Parameters.pairingRange/2});
+			mv.noStroke();
+			mv.fill(6*neighbors.size()%255, 2*neighbors.size()%255, 6*neighbors.size()%255);		
+			mv.mesh.drawVertex(s.getVertex().getPoint());
+		}
 	}
 }
